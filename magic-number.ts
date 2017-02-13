e0bac28ce71c31bd9f775b28cdda290b04b62963
Sam Saint-Pettersen <s.stpettersen+github@gmail.com>
2017-02-13 21:42:00 +0000

    ...

--- magic-number.ts
+++ magic-number.ts
@@ -18,29 +18,18 @@ class MagicNumber {
 	private static mimes: string[];
 	private static ids: string[];
 
-	private static loadFileTypes(): void {
-		//if(fs.existsSync('file.types')) {
-		try {
-			MagicNumber.mimes = new Array<string>();
-			MagicNumber.ids = new Array<string>();
-			var data: any = fs.readFileSync('file.types');
-			var lines: string[] = data.toString().split('\n');
-			for(var i: number = 0; i < lines.length; i++) {
-				var mi: string[] = lines[i].split(':');
-				if(mi[0] != '') {
-					MagicNumber.mimes.push(mi[0]);
-					MagicNumber.ids.push(mi[1]);
-				}
+	private static loadFileTypes(definitions: string = "file.types"): void {
+		MagicNumber.mimes = new Array<string>();
+		MagicNumber.ids = new Array<string>();
+		var data: any = fs.readFileSync(definitions);
+		var lines: string[] = data.toString().split('\n');
+		for(var i: number = 0; i < lines.length; i++) {
+			var mi: string[] = lines[i].split(':');
+			if(mi[0] != '') {
+				MagicNumber.mimes.push(mi[0]);
+				MagicNumber.ids.push(mi[1]);
 			}
 		}
-		catch(e) {
-			console.error('magic-number error:');
-			console.error(e.status);
-		}
-		/*else {
-			console.log('Error in magicnumber module: file.types file missing.')
-			process.exit(-1);
-		}*/
 	}
 
 	public static detectFile(file: string): string {
