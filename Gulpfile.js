4a6fbfe96c70e2cf527e3baaef99f2cf2bcadd61
Sam Saint-Pettersen <s.stpettersen+github@gmail.com>
2017-02-13 20:59:17 +0000

    new branch

--- Gulpfile.js
+++ Gulpfile.js
@@ -4,6 +4,7 @@
 var gulp = require('gulp'),
       fs = require('fs'),
      tsc = require('gulp-typescript'),
+   mocha = require('gulp-mocha'),
  replace = require('gulp-replace');
 
 gulp.task('default', function() {
@@ -15,6 +16,11 @@ gulp.task('default', function() {
   .pipe(replace(/\/{3}\s*<reference path=.*\/>\r*\n*/g, ''))
   .pipe(gulp.dest('.'));
 });
+
+gulp.task('test', function() {
+  return gulp.src('magic-number.test.js', {read: false})
+  .pipe(mocha({reporter: 'min'}));
+})
 
 gulp.task('clean', function() {
   fs.unlinkSync('magic-number.js');
